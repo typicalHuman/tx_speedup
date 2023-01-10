ff0886d92e94552928aa46afb2592c20145d788c
typicalHuman <reallytovald@gmail.com>
2023-01-10 21:09:53 +0600

    form done

---
+++ src/logic/enums/SpeedupType.ts
@@ -0,0 +1,4 @@
+export enum SpeedupType {
+  Percent,
+  Absolute,
+}
--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,41 +1,33 @@
-import {MemoryRouter as Router, Routes, Route} from 'react-router-dom';
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import './App.css';
-import {ethers} from 'ethers';
+import { ethers } from 'ethers';
 import FileInput from './components/FileInput';
-import {useState} from 'react';
-import {MainForm} from './components/MainForm';
+import { useState } from 'react';
+import { MainForm } from './components/MainForm';
+import { SpeedupType } from 'logic/enums/SpeedupType';
 
-var prov = new ethers.providers.JsonRpcProvider(
-  'https://darkforestsecond.xyz/dNuaLgyKssrk9TEPPz9NTyQT'
-);
-
-prov
-  .getTransaction(
-    '0x7e41d74b24420746df911dccb5a4697269467abd0404bfa716ade961e6bfdcea'
-  )
-  .then(console.log);
 function Home() {
-    const [fileText,
-        setTextValue] = useState('');
-    return (
-        <div>
-            <FileInput setTextValue={setTextValue}/>
-            <div className='home'>
-                <div className="form-div">
-                    <MainForm/>
-                </div>
-            </div>
+  const [fileText, setTextValue] = useState('');
+  const [speedupType, setSpeedupType] = useState(SpeedupType.Absolute);
+  return (
+    <div>
+      <FileInput setTextValue={setTextValue} />
+      <div className="home">
+        <div className="form-div">
+          <MainForm speedupType={speedupType} setSpeedupType={setSpeedupType} />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={< Home />}/>
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
