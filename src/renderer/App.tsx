ef104419b69d9b448d67a0f66774088eb9c968f1
typicalHuman <reallytovald@gmail.com>
2023-01-10 19:35:23 +0600

    tailwind templtae

---
+++ src/renderer/App.tsx
@@ -0,0 +1,21 @@
+import {MemoryRouter as Router, Routes, Route} from 'react-router-dom';
+import icon from '../../assets/icon.svg';
+import './App.css';
+
+const Hello = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        </div>
+    );
+};
+
+export default function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={< Hello />}/>
+            </Routes>
+        </Router>
+    );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mode: 'jit',
+  content: ['./src/renderer/**/*.{js,jsx,ts,tsx,ejs}'],
+  theme: {},
+  variants: {},
+  plugins: [],
+};
